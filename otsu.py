231aa9cb7ab77dc255123496bef07e7dc3f1d854
Douglas de Jesus <douglasdejesus92@gmail.com>
2022-11-18 13:53:37 -0500

    Compute normalized histogram

--- otsu.py
+++ otsu.py
@@ -8,18 +8,37 @@ import numpy as np
 def grayscale(r, g, b):
     return round(0.299 * r + 0.587 * g + 0.114 * b)
 
+def normalized_histogram(gray_values):
+    histogram = {}
+    for i in range(256):
+        histogram[i] = 0
 
-img = Image.open('test_iamges/tiger1.bmp')
+    for g in gray_values:
+        histogram[g] += 1
 
-gray_values = [grayscale(rgb[0], rgb[1], rgb[2]) for rgb in img.getdata()]
+    # Normalize
+    for i in range(256):
+        histogram[i] /= len(gray_values)
+    
+    return histogram
 
-gray_values = np.uint8(np.reshape(gray_values, (img.height, img.width)))
-img2 = Image.fromarray(gray_values, mode='L')
-img2.show()
+def main():
+    img = Image.open('../test_images/tiger1.bmp')
 
-for t1 in range(0, 255):
-    print(f"2 regions, [0, {t1}] [{t1+1}, 255]")
-    for t2 in range(t1+1, 255):
-        print(f"3 regions,  [0, {t1}] [{t1+1}, {t2}] [{t2+1}, 255]")
-        for t3 in range(t2+1, 255):
-            print(f"4 regions,  [0, {t1}] [{t1+1}, {t2}] [{t2+1}, {t3}] [{t3+1}, 255]")
+    gray_values = [grayscale(rgb[0], rgb[1], rgb[2]) for rgb in img.getdata()]
+
+    histogram = normalized_histogram(gray_values)
+
+    # gray_values = np.uint8(np.reshape(gray_values, (img.height, img.width)))
+    # img2 = Image.fromarray(gray_values, mode='L')
+    # img2.show()
+
+    for t1 in range(0, 255):
+        print(f"2 regions, [0, {t1}] [{t1+1}, 255]")
+        for t2 in range(t1+1, 255):
+            print(f"3 regions,  [0, {t1}] [{t1+1}, {t2}] [{t2+1}, 255]")
+            for t3 in range(t2+1, 255):
+                print(f"4 regions,  [0, {t1}] [{t1+1}, {t2}] [{t2+1}, {t3}] [{t3+1}, 255]")
+
+if __name__ == "__main__":
+    main()
