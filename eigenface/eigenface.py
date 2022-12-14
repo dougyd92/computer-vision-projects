562b144d73a6564a271388cf1a6a066926b25446
Douglas de Jesus <douglasdejesus92@gmail.com>
2022-12-14 00:41:54 -0500

    Cleanup

--- eigenface/eigenface.py
+++ eigenface/eigenface.py
@@ -2,45 +2,37 @@
 # drd8913@nyu.edu
 # N14928011
 
-import os
+import argparse
 import numpy as np
+import os
+
 from PIL import Image
 
 
-# Todo want a way to have code reuse
-# but also still access width, height, and filenames
-# def load_images(directory):
-#     images = []
-#     files = os.listdir(directory)
-#     for filename in files:
-#         img_path = os.path.join(directory, filename)
-#         img = Image.open(img_path)
-#         images.append(img)
-#     return images
-
-# def images_to_col_vectors(images_arr):
-#     image_vectors = []
-#     for img in images_arr:
-#         column_vector = np.array(img.getdata())[:, np.newaxis]
-#         image_vectors.append(column_vector)
-#     return image_vectors
-
-
 def output_image(gray_values, height, width, directory, filename):
-    pixels = np.uint8(np.reshape(gray_values, (height, width)))
-    img = Image.fromarray(pixels, mode='L')
-    img_path = os.path.join(directory, filename)
-    img.save(img_path)
+    if directory is not None:
+        pixels = np.uint8(np.reshape(gray_values, (height, width)))
+        img = Image.fromarray(pixels, mode='L')
+        img_path = os.path.join(directory, filename)
+        img.save(img_path)
 
 
 def main():
-    # todo argparse
-    training_dir = "../Project2/Face dataset/Training"
-    test_dir = "../Project2/Face dataset/Testing"
-    output_dir = "../Project2/Face dataset/Output"
+    parser = argparse.ArgumentParser(
+        prog = 'eigenface.py',
+        description = """Facial recognition using eigenfaces. Specify a set of training
+                         set images and another set of test images to be classified.""")
+    parser.add_argument('training_dir', help='Directory containing images for the training set.')
+    parser.add_argument('test_dir', help='Directory containing images for the test set.')
+    parser.add_argument('-o', '--output_dir', required=False, 
+        help='Directory where mean face and eigenfaces will be stored.')
+    args = parser.parse_args()
 
-    # training_images = load_images(training_dir)
-    # training_image_vectors = images_to_col_vectors(training_images) # todo see above
+    training_dir = args.training_dir
+    test_dir = args.test_dir
+    output_dir = args.output_dir
+
+    # Load training images
     training_files = os.listdir(training_dir)
     training_images = []
     for filename in training_files:
@@ -52,7 +44,8 @@ def main():
     height = training_images[0].height
     width = training_images[0].width
 
-    training_image_vectors = [] # todo remove this
+    # Get training images in column vector form
+    training_image_vectors = []
     for img in training_images:
         column_vector = np.array(img.getdata())[:, np.newaxis]
         training_image_vectors.append(column_vector)
@@ -79,6 +72,9 @@ def main():
     # i.e. the eigenfaces
     L = A.T @ A
     w, V = np.linalg.eig(L)
+    eigenpairs = {w[i] : V[:, i] for i in range(M)}
+    # Sort the eigenvectors by descending eigenvalue
+    V = np.vstack([eigenpairs[eigenval] for eigenval in sorted(w, reverse=True)]).T
     U = A @ V
 
     for i in range(M):
@@ -87,32 +83,34 @@ def main():
     # The columns of this M x M matrix are the projections of the training images
     # onto the face space, ie the values are the Eigenface coefficients 
     training_coeffs = np.array([U.T @ A[:, i] for i in range(M)]).T
-    print("Eigenface coefficients of the training images") #todo formatting
-    print(training_coeffs)
+    for i in range(M):
+        print(f"Training image {training_files[i]} coefficients")
+        print(training_coeffs[:, i].reshape(-1,1), "\n")
 
-
-    # Recognition
-    # test_images = load_images(test_dir)
-    # test_image_vectors = images_to_col_vectors(test_images) # todo see above
+    # Load test images
     test_files = os.listdir(test_dir)
-    test_images = []
+    test_image_vectors = []
     for filename in test_files:
         img_path = os.path.join(test_dir, filename)
         img = Image.open(img_path)
-        test_images.append(img)
-
-    test_image_vectors = [] # todo remove this
-    for img in test_images:
         column_vector = np.array(img.getdata())[:, np.newaxis]
         test_image_vectors.append(column_vector)
 
+    # Classify test images
     for i, test_img in enumerate(test_image_vectors):
         mean_adjusted_img = test_img - mean_face
+
+        # Project the test image onto the face space to find the eigenface coefficients
         projection = U.T @ mean_adjusted_img #todo output these coefficients
+
+        # Find nearest neighbor in the training set
         differences = training_coeffs - projection
         distances = np.linalg.norm(differences, axis=0)
         classification = np.argmin(distances)
-        print(f"Classify test image {i} {test_files[i]} as training image {classification} {training_files[classification]}")
+
+        print(f"Test image {test_files[i]} coefficients")
+        print(projection)
+        print(f"Closest match is training image {training_files[classification]}\n")
 
         reconstructed = U @ projection
         output_image(reconstructed, height, width, output_dir, f"reconstructed_{i}.jpg")
